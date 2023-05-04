f9c8e90d841e2b6542bb58337c72216a8e869776
Jason Chow <jason.k.chow@vanderbilt.edu>
2023-05-04 11:44:22 -0500

    new training for ecoCUB

--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import os
 import numpy as np
+import ecoset
 
 
-def create_dataset(directory):
+def create_nested_dataset(directory, size=224, channel_first=False, batch_size=32):
     """
     Create tf.Data.Dataset objects from the train and val directories, assumes
     that we have one directory that has subdirectories so we will have multi-
-    class classification.
+    class classification. The dataset will be resized to size x size. The
+    dataset will be batched and shuffled.
     """
     # List folders in trainDir
     basicClasses = os.listdir(directory)
@@ -18,7 +20,7 @@ def create_dataset(directory):
     for folder in basicClasses:
         files = os.listdir(os.path.join(directory, folder))
         if os.path.isdir(os.path.join(directory, folder, files[0])):
-            nSub = len(files) + 1 # +1 for non-bird
+            nSub = len(files) + 1  # +1 for non-bird
             break
 
     imgPaths = []
@@ -70,17 +72,18 @@ def create_dataset(directory):
         # Cast to float
         x = tf.cast(x, tf.float32)
 
-        # Resize to 128x128
-        x = tf.keras.preprocessing.image.smart_resize(x, (128, 128))
+        # Resize
+        x = tf.keras.preprocessing.image.smart_resize(x, (size, size))
 
         # Center features
         x = 2 * (x / 255 - 0.5)
 
         # Transpose to channel first format
-        x = tf.transpose(x, (2, 0, 1))
+        if channel_first:
+            x = tf.transpose(x, (2, 0, 1))
 
         # One-hot encode labels
-        y = (tf.one_hot(y[0], nBasic + 1), tf.one_hot(y[1], nSub + 1))
+        y = (tf.one_hot(y[0], nBasic), tf.one_hot(y[1], nSub))
 
         return x, y
 
@@ -92,16 +95,296 @@ def create_dataset(directory):
                 tf.TensorSpec(shape=(2,), dtype=tf.int32),
             ),
         )
+        .shuffle(len(imgPaths))
         .map(_parse_image)
-        .batch(32)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
     )
 
     # Calculate category weights
-    basicWeights = np.sum(basicCounts) / (nBasic + 1) / basicCounts
-    subWeights = np.sum(subCounts) / (nSub + 1) / subCounts
+    basicWeights = np.sum(basicCounts) / (nBasic * basicCounts)
+    subWeights = np.sum(subCounts) / (nSub * subCounts)
+
+    # Convert weights to tensors
+    basicWeights = tf.convert_to_tensor(basicWeights, dtype=tf.float32)
+    subWeights = tf.convert_to_tensor(subWeights, dtype=tf.float32)
+
+    # Print class counts
+    print(
+        f"Found {len(imgPaths)} images belonging to {nBasic} basic classes and {nSub} subordinate classes."
+    )
 
     return ds, basicWeights, subWeights
 
 
+def create_flat_dataset(
+    directory, size=224, channel_first=False, batch_size=32, augment=False
+):
+    """
+    Return a dataset and class weights from directory where all images are
+    scaled to size x size.
+    """
+    # List folders in directory
+    classes = os.listdir(directory)
+    classes.sort()
+
+    # Create lists
+    imgPaths = []
+    labels = []
+    classCounts = np.array([])
+    for i, folder in enumerate(classes):
+        # List files in folder
+        files = os.listdir(os.path.join(directory, folder))
+        files.sort()
+
+        imgCount = 0
+        for file in files:
+            imgPaths.append(os.path.join(directory, folder, file))
+            labels.append(i)
+            imgCount += 1
+
+        classCounts = np.append(classCounts, imgCount)
+
+    # Calculate class weights
+    weights = np.sum(classCounts) / (len(classes) * classCounts)
+    weights = tf.convert_to_tensor(weights, dtype=tf.float32)
+
+    def _parse_image(x, y):
+        # Decode image
+        x = tf.io.read_file(x)
+        x = tf.io.decode_image(x, channels=3)
+
+        # Cast to float
+        x = tf.cast(x, tf.float32)
+
+        # Augment and resize
+        if augment:
+            # Resize to slightly larger
+            x = tf.keras.preprocessing.image.smart_resize(x, (size + 32, size + 32))
+
+            # Random crop
+            x = tf.image.random_crop(x, (size, size, 3))
+
+            # Random flip
+            x = tf.image.random_flip_left_right(x)
+        else:  # No augment
+            # Resize
+            x = tf.keras.preprocessing.image.smart_resize(x, (size, size))
+
+        # Center features
+        x = 2 * (x / 255 - 0.5)
+
+        # Transpose to channel first format
+        if channel_first:
+            x = tf.transpose(x, (2, 0, 1))
+
+        # One-hot encode labels
+        y = tf.one_hot(y, len(classes))
+
+        return x, y
+
+    ds = (
+        tf.data.Dataset.from_generator(
+            lambda: zip(imgPaths, labels),
+            output_signature=(
+                tf.TensorSpec(shape=(), dtype=tf.string),
+                tf.TensorSpec(shape=(), dtype=tf.int32),
+            ),
+        )
+        .shuffle(len(imgPaths))
+        .map(_parse_image)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+    # Print dataset info
+    print(f"Found {len(imgPaths)} images belonging to {len(classes)} classes.")
+
+    return ds, weights
+
+
+def make_expert_model(
+    model,
+    layer_idx,
+    num_layers,
+    n_thaw,
+    basicWeights=None,
+    subWeights=None,
+    loss_weights=[1, 1],
+):
+    """
+    Return a copy of the model with an additional expert branch added at
+    layer_idx. The expert branch will be num_layers layers deep. All old layers
+    behind layer_idx - n_thaw will be frozen.
+    """
+    # Turn negative indices positive
+    if layer_idx < 0:
+        layer_idx = len(model.layers) + layer_idx
+
+    # Freeze layers
+    for layer in model.layers[: layer_idx - n_thaw]:
+        print(f"Freezing layer {layer.name}")
+        layer.trainable = False
+
+    # Get output
+    outputs = model.outputs
+
+    # Get the layer to add to
+    layer = model.layers[layer_idx]
+    print(f"Adding branch to layer {layer.name}")
+    x = layer.output
+
+    for _ in range(num_layers):
+        x = tf.keras.layers.Conv2D(1024, 3, activation="relu", padding="same")(x)
+
+    # Add expert branch
+    x = tf.keras.layers.Conv2D(201, 5, activation=None, padding="same")(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Softmax()(x)
+
+    # Create new model
+    model = tf.keras.Model(inputs=model.input, outputs=outputs + [x])
+
+    if basicWeights is None or subWeights is None:
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss=[
+                tf.keras.losses.CategoricalCrossentropy(),
+                tf.keras.losses.CategoricalCrossentropy(),
+            ],
+            metrics=["accuracy"],
+            loss_weights=loss_weights,
+        )
+
+    else:
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss=[
+                weighted_cce(basicWeights),
+                weighted_cce(subWeights),
+            ],
+            metrics=["accuracy"],
+            loss_weights=loss_weights,
+        )
+
+    return model
+
+
+class weighted_cce(tf.keras.losses.Loss):
+    def __init__(self, weights, **kwargs):
+        super().__init__(**kwargs)
+        self.weights = weights
+
+    def call(self, y_true, y_pred, sample_weight=None):
+        weights = tf.gather(self.weights, tf.argmax(y_true, axis=-1))
+
+        return tf.keras.losses.CategoricalCrossentropy()(y_true, y_pred, weights)
+
+
+def train_ecocub_model(model, class_weights, lr, callbacks=[], initial_train=False):
+    """
+    Take an AlexNet model and perform transfer learning on it to classify the
+    ecoCUB dataset.
+    """
+    # Freeze all layers
+    for layer in model.layers:
+        layer.trainable = False
+
+    # Get model output at fc dropout layer
+    x = model.layers[-5].output
+
+    # Add new classification layer
+    weightInit = tf.keras.initializers.TruncatedNormal(stddev=0.005)
+    x = tf.keras.layers.Conv2D(
+        764,
+        (1, 1),
+        padding="same",
+        activation=None,
+        name="birdFC",
+        kernel_regularizer=tf.keras.regularizers.l2(0.001),
+        kernel_initializer=weightInit,
+        bias_initializer=tf.keras.initializers.zeros(),
+    )(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Softmax()(x)
+
+    # Create new model
+    model = tf.keras.Model(inputs=model.input, outputs=[x])
+
+    # Turn class weights into dictionary
+    class_weights = {i: class_weights[i] for i in range(len(class_weights))}
+
+    if initial_train:
+        print("Training one epoch to line up clasification layer")
+        # Train one epoch to line up the new classification layer
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(epsilon=0.1),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+        )
+
+        model.summary()
+
+        # Fit one epoch
+        model.fit(
+            trainDs,
+            epochs=1,
+            validation_data=valDs,
+            class_weight=class_weights,
+        )
+
+    # Unfreeze penultimate layer and add regularizer
+    model.layers[-6].trainable = True
+    model.layers[-6].kernel_regularizer = tf.keras.regularizers.l2(0.001)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr, epsilon=0.1),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy", "top_k_categorical_accuracy"],
+    )
+
+    model.summary()
+
+    # Train model
+    fit = model.fit(
+        trainDs,
+        epochs=10,
+        validation_data=valDs,
+        class_weight=class_weights,
+        callbacks=callbacks,
+    )
+
+    return fit
+
+
 if __name__ == "__main__":
-    create_dataset("./images/ecoset/train")
+    # Training seed
+    seed = 1
+    tf.random.set_seed(seed)
+
+    # Create dataset
+    trainDs, weights = create_flat_dataset("./images/ecoCUB/train")
+    valDs, _ = create_flat_dataset("./images/ecoCUB/val")
+
+    weightPath = f"./models/AlexNet/ecoset_training_seeds_01_to_10/training_seed_{seed:02}/model.ckpt_epoch89"
+    model = ecoset.make_alex_net_v2(
+        weights_path=weightPath,
+        softmax=True,
+    )
+
+    # Make callbacks
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        f"./models/deepCats/AlexNet/seed{seed:02}/epoch{{epoch:02d}}-val_loss{{val_loss:.2f}}.hdf5",
+        monitor="val_loss",
+        save_freq="epoch",
+    )
+    csvLogger = tf.keras.callbacks.CSVLogger(
+        f"./models/deepCats/AlexNet/seed{seed:02}/training.csv"
+    )
+    callbacks = [checkpoint, csvLogger]
+
+    # Train model
+    fit = train_ecocub_model(
+        model=model, class_weights=weights, lr=0.0001, callbacks=callbacks
+    )
